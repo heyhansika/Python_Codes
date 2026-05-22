ae52db68be75bed7fdbc4538e84007ac036e9f14
Hansika_Agrawal <hansikaagrawal930@gmail.com>
2026-05-22 17:59:19 +0530

    Create backtrack.py

---
+++ backtrack.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partition(self, s):
+
+        result = []
+
+        def isPalindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(start, path):
+
+            if start == len(s):
+                result.append(path[:])
+                return
+
+            for end in range(start + 1, len(s) + 1):
+
+                substring = s[start:end]
+
+                if isPalindrome(substring):
+
+                    path.append(substring)
+
+                    backtrack(end, path)
+
+                    path.pop()
+
+        backtrack(0, [])
+
+        return result
