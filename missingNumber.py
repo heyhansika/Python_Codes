15cb9bbd27be3c025750841753259aac5c3f18a0
Hansika_Agrawal <hansikaagrawal930@gmail.com>
2026-08-26 14:58:48 +0530

    Implement missing number finder in missingNumber.py

---
+++ missingNumber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums):
+
+        result = len(nums)
+
+        for i in range(len(nums)):
+            result ^= i
+            result ^= nums[i]
+
+        return result
