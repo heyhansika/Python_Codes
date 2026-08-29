73838223cedb27452ab93a2c69979c17f85b2128
Hansika_Agrawal <hansikaagrawal930@gmail.com>
2026-08-29 18:31:07 +0530

    Implement pivot index calculation in pivotIndex.py

---
+++ pivotIndex.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotIndex(self, nums):
+
+        total = sum(nums)
+        leftSum = 0
+
+        for i in range(len(nums)):
+
+            rightSum = total - leftSum - nums[i]
+
+            if leftSum == rightSum:
+                return i
+
+            leftSum += nums[i]
+
+        return -1
