955254c848c40d3a160de50c09606da1b933ed23
Hansika_Agrawal <hansikaagrawal930@gmail.com>
2026-09-02 15:45:25 +0530

    Implement isAnagram function in isAnagram.py

---
+++ isAnagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s, t):
+
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for num in count:
+            if num != 0:
+                return False
+
+        return True
